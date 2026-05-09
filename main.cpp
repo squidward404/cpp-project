99af4fe80362f18dc18da8c3689e7d5bfca7ec4b
Befikir Shimelis <161409855+squidward404@users.noreply.github.com>
2026-05-09 16:35:05 +0300

    updated main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+struct supplier{
+int supplier_id;
+string supplier_name;
+string phone;
+};
+struct product{
+int product_id;
+int supplier_id;
+string name;
+double price;
+};
+struct customer{
+int customer_id;
+string name;
+string phone;
+};
+struct inventory_item{
+int inventory_id;
+int product_id;
+int quantity;
+};
+struct sale{
+int sale_id;
+int product_id;
+int customer_id;
+int quantity_sold;
+int sold_date;
+};
+int main(){
+    cout<<"Hello, World!"<<endl;
+    return 0;
+}
