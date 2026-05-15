df0751efc6a8767d5b56b9e0b08c589582f63a45
Befikir Shimelis <161409855+squidward404@users.noreply.github.com>
2026-05-15 11:15:30 +0300

    Move DB exports and .gitignore to repo root

---
+++ dump.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE products
+(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT,
+    price REAL NOT NULL,
+    quantity INTEGER NOT NULL,
+    expiry_date TEXT
+);
+INSERT INTO products VALUES(1000,'Sample Product','Test',9.99000000000000021,5,'2026-12-31');
+COMMIT;
